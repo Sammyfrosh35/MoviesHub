0bc778d9b4f58e31e6b01198406da693160f7ece
Sammyfrosh35 <133043517+Sammyfrosh35@users.noreply.github.com>
2023-12-11 00:44:05 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("batman");
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
